177487e7dfc0f58f04b410e6864f6c55064ef2c2
Maj-mondo <diattamfall@gmail.com>
2019-01-18 18:03:58 +0000

    ooo

--- founisseur/index.php
+++ founisseur/index.php
@@ -1,2 +1,41 @@
+<?php include '../includes/header-site.php'; ?>
+
+<section id="menu-area">
+  <nav class="navbar navbar-default main-navbar" role="navigation">
+    <div class="container">
+      <div class="navbar-header">
+        <!-- FOR MOBILE VIEW COLLAPSED BUTTON -->
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <!-- LOGO -->
+        <a class="navbar-brand logo" href="index.php"><img src="/assets/images/logo.png" alt="logo"></a>
+      </div>
+      <div id="navbar" class="navbar-collapse collapse">
+        <ul id="top-menu" class="nav navbar-nav main-nav menu-scroll">
+          <li class="active"><a href="/" class="less-padding">ACCUEIL</a></li>
+          <li><a href="/catalogue.php" class="less-padding">CATALOGUE</a></li>
+          <li><a href="/founisseur/index.php" class="less-padding">Founisseurs</a></li>
+          <li><a href="/vehicules.php" class="less-padding">Véhicules</a></li>
+        <!--  TODO: Only admin can see -->
+          <li><a href="#client" class="less-padding">Clients</a></li>
+          <li><a href="blog.php" class="less-padding">BLOG</a></li>
+          <li><a href="log-agent.php" class="less-padding">ESPACE AGENCE</a></li>
+          <li><a href="#contact" class="less-padding">CONTACT</a></li>
+        </ul>
+      </div>
+      <!--/.nav-collapse -->
+      <div class="search-area">
+        <form action="">
+          <input id="search" name="search" type="text" placeholder="Je cherche ...">
+          <input id="search_submit" value="Rechercher" type="submit">
+        </form>
+      </div>
+    </div>
+  </nav>
+</section>
 <?php
 echo "fournisseur index";
--- includes/header-admin.php
+++ includes/header-admin.php
@@ -1,4 +1,4 @@
-<?php require_once 'conn.php'; ?>
+<?php require_once 'db/conn.php'; ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -188,74 +188,13 @@
                       <span class=" fa fa-angle-down"></span>
                     </a>
                     <ul class="dropdown-menu dropdown-usermenu pull-right">
-                      <li><a href="admin.profile.php"> Profile</a></li>
                       <li><a href="log-agent.php"><i class="fa fa-sign-out pull-right"></i> Log Out</a></li>
                     </ul>
                   </li>
-
                   <li role="presentation" class="dropdown">
-                    <a href="javascript:;" class="dropdown-toggle info-number" data-toggle="dropdown" aria-expanded="false">
+                    <a href="admin.inbox.php" class="dropdown-toggle info-number">
                       <i class="fa fa-envelope-o"></i>
-                      <span class="badge bg-green">6</span>
                     </a>
-                    <ul id="menu1" class="dropdown-menu list-unstyled msg_list" role="menu">
-                      <li>
-                        <a>
-                          <span class="image"><img src="assets/images/img.png" alt="Profile Image" /></span>
-                          <span>
-                            <span>John Smith</span>
-                            <span class="time">3 mins ago</span>
-                          </span>
-                          <span class="message">
-                            Film festivals used to be do-or-die moments for movie makers. They were where...
-                          </span>
-                        </a>
-                      </li>
-                      <li>
-                        <a>
-                          <span class="image"><img src="assets/images/img.png" alt="Profile Image" /></span>
-                          <span>
-                            <span>John Smith</span>
-                            <span class="time">3 mins ago</span>
-                          </span>
-                          <span class="message">
-                            Film festivals used to be do-or-die moments for movie makers. They were where...
-                          </span>
-                        </a>
-                      </li>
-                      <li>
-                        <a>
-                          <span class="image"><img src="assets/images/img.png" alt="Profile Image" /></span>
-                          <span>
-                            <span>John Smith</span>
-                            <span class="time">3 mins ago</span>
-                          </span>
-                          <span class="message">
-                            Film festivals used to be do-or-die moments for movie makers. They were where...
-                          </span>
-                        </a>
-                      </li>
-                      <li>
-                        <a>
-                          <span class="image"><img src="assets/images/img.png" alt="Profile Image" /></span>
-                          <span>
-                            <span>John Smith</span>
-                            <span class="time">3 mins ago</span>
-                          </span>
-                          <span class="message">
-                            Film festivals used to be do-or-die moments for movie makers. They were where...
-                          </span>
-                        </a>
-                      </li>
-                      <li>
-                        <div class="text-center">
-                          <a href="admin.inbox.php">
-                            <strong>See All Alerts</strong>
-                            <i class="fa fa-angle-right"></i>
-                          </a>
-                        </div>
-                      </li>
-                    </ul>
                   </li>
                 </ul>
               </nav>
